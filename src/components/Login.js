d1a909b959d75e78ca458a3c7ccc2b086a2449e2
Brigituciutyte <brigituciutyte@Lorenass-MBP.lan>
2023-04-18 18:11:06 +0300

    change login style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 import axios from 'axios';
 import { MessagesContext } from './context/MessagesContext';
@@ -25,12 +25,12 @@ const Login = () => {
         axios.post('http://127.0.0.1:8000/api/login', data, { headers: headers })
             .then(resp => {
                 auth.login(resp.data.data.user, resp.data.data.token)
-                navigate('/')
-                setAlert({message: 'Login successfully!'})
+                navigate('/user/posts')
+                setAlert({ message: 'Login successfully!' })
             })
             .catch(err => {
                 // console.log(err.response.data.message)
-                setAlert({message: err.response.data.message, warning: true})
+                setAlert({ message: err.response.data.message, warning: true })
             })
 
     }
@@ -38,35 +38,56 @@ const Login = () => {
 
     return (
         <>
-    
-            <article className="article login w-75">
-                <h1>Please Login</h1>
-                <form
-                    onSubmit={login}
-                >
-                    <div className="mb-3">
-                        <label>Email address:</label>
-                        <input
-                            type="email"
-                            name="email"
-                            onChange={handleData}
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label>Password:</label>
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={handleData}
-                            className="form-control"
-                        />
-                    </div>
-                    <div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </div>
-                </form>
-            </article>
+
+
+            <h5 className='text-center'>Login Form</h5>
+
+            <form
+                className='d-flex flex-column align-items-center'
+                onSubmit={login}
+            >
+
+                <div class="form-floating col-4 mb-4">
+                    <input
+                        className="form-control"
+                        placeholder="name@example.com"
+                        type="email"
+                        name="email"
+                        onChange={handleData}
+                    />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating col-4 mb-2">
+                    <input
+                        className="form-control"
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        onChange={handleData}
+                    />
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div className='d-grid gap-2 col-4 mx-auto mb-3'>
+                    <button type="submit" className="btn btn-primary mt-3"> Sign in</button>
+                </div>
+
+                <div className='text-center'>
+                    <p>
+                        Not a member? <NavLink className='' to="/register">Register</NavLink>
+                    </p>
+
+
+                </div>
+            </form>
+
+
+
+
+
+            {/* </article> */}
+
+
+
 
         </>
     );
