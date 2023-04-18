c9029fdb0f5db5fad1ad046ad71eb06b657b4976
Brigituciutyte <brigituciutyte@Lorenass-MBP.lan>
2023-04-18 18:11:34 +0300

    change register component style

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 
 const Register = () => {
@@ -28,67 +28,87 @@ const Register = () => {
         e.preventDefault()
         axios.post('http://127.0.0.1:8000/api/register', userInfo)
             .then(resp => {
+                console.log(resp.data)
                 auth.login(resp.data.data.user, resp.data.data.token)
                 navigate("/");
             })
-        // .catch((err) => {
-        // //     // if (err.response) {
-        //         console.log(err);
-        // //     //   }
-        //   });
+            .catch(error => {
+                console.error(error.response.data);
+            });
 
     }
 
 
+
     return (
         <>
-            <article className="article w-75">
-                <h1>Please Register</h1>
-                <form
-                    onSubmit={handleSubmit}
-                >
-                    <div className="mb-3">
-                        <label>Name</label>
-                        <input
-                            type="text"
-                            name="name"
-                            onChange={handleData}
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label>Email address:</label>
-                        <input
-                            type="email"
-                            name="email"
-                            onChange={handleData}
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label>Password:</label>
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={handleData}
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label>Password confirmation:</label>
-                        <input
-                            type="password"
-                            name="password_confirmation"
-                            onChange={handleData}
-                            className="form-control"
-                        />
-                    </div>
 
-                    <div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </div>
-                </form>
-            </article>
+            <h5 className='text-center'>Register form</h5>
+
+            <form
+                className='d-flex flex-column align-items-center'
+                onSubmit={handleSubmit}
+            >
+                <div className="form-floating col-4 mb-4">
+                    <input
+                        className="form-control"
+                        placeholder="name"
+                        type="text"
+                        name="name"
+                        onChange={handleData}
+                    />
+                    <label for="floatingInput">Name</label>
+                </div>
+                <div class="form-floating col-4 mb-4">
+
+                    <input
+                        className="form-control"
+                        placeholder="name@example.com"
+                        type="email"
+                        name="email"
+                        onChange={handleData}
+                    />
+                    <label for="floatingInput">Email address</label>
+
+                </div>
+                <div className="form-floating col-4 mb-2">
+                    <input
+                        className="form-control"
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        onChange={handleData}
+                    />
+                    <label for="floatingPassword">Password</label>
+
+                </div>
+                <div className="form-floating col-4 mb-2">
+                    <input
+                        className="form-control"
+                        placeholder="Password Confirmation"
+                        type="password"
+                        name="password_confirmation"
+                        onChange={handleData}
+
+                    />
+                    <label for="floatingPasswordConfirmation">Password Confirmation</label>
+
+                </div>
+
+                <div className='d-grid gap-2 col-4 mx-auto mb-3'>
+                    <button type="submit" className="btn btn-primary mt-3"> Sign in</button>
+                </div>
+
+                <div className='text-center'>
+                    <p>
+                        Already a member? <NavLink className='' to="/login">Login</NavLink>
+                    </p>
+
+
+                </div>
+
+            </form>
+
         </>
     );
 };
