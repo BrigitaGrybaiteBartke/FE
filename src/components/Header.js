9b7fbd540228e4990afaef75d0d810a0c00b20e3
Brigituciutyte <brigituciutyte@Lorenass-MBP.lan>
2023-04-18 18:28:39 +0300

    change header component style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,56 @@
 import { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
+import { IoMdPerson } from 'react-icons/io';
+import { RxPerson } from 'react-icons/rx';
 
 
 const Header = () => {
 
     const auth = useContext(AuthContext);
 
-
-
-
     return (
         <>
 
-
             <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary bg-light" background="#e3f2fd">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" to="/">Navbar</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
 
-                    {/* main navigation */}
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                            {/* home page */}
                             <li className="nav-item">
                                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/home">Home</NavLink>
                             </li>
-
-                            {/* Posts */}
                             <li className="nav-item">
                                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/posts">Posts</NavLink>
                             </li>
 
                             {auth.isLoggedin() ? (
                                 <li className="nav-item">
-                                    <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/admin">Dashboard</NavLink>
+                                    <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/user/posts">Dashboard</NavLink>
                                 </li>
                             ) : (null)}
                         </ul>
 
-                        {/* USER */}
                         {!auth.isLoggedin() ? (
                             <ul className='navbar-nav d-flex'>
-                                <li className="nav-item">
-                                    <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/register">Register</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/login">Login</NavLink>
+                                <li className="nav-item dropdown dropstart">
+                                    <Link className="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <RxPerson />
+                                    </Link>
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <NavLink className="nav-link" aria-current="page" to="/login">Login</NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink className="nav-link" aria-current="page" to="/register">Register</NavLink>
+                                        </li>
+                                    </ul>
                                 </li>
                             </ul>
                         ) : (
                             <ul className='navbar-nav d-flex'>
                                 <li className="nav-item">
-                                    <span className="nav-link mx-4">{`Hello ${auth.getUser().name}`}</span>
+                                    <span className="nav-link mx-4">{`Hello, ${auth.getUser().name}`}</span>
                                 </li>
                                 <li className="nav-item">
                                     <NavLink
@@ -69,23 +64,10 @@ const Header = () => {
                                 </li>
                             </ul>
                         )}
-
                     </div>
                 </div>
             </nav>
 
-            {/* <div className='section'>
-                <div className='container'>
-                    <div className='row justify-content-center m-5'>
-                        <div className='col text-center'>
-                            <div className='section-title'>
-                                <h2 className='text-uppercase'>Here we are</h2>
-                                <p>All about today's news</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </>
 
     )
