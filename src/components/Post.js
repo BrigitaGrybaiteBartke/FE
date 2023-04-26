214f2171696e0e558455fe852b51d1a48b9f6a7c
Brigituciutyte <brigituciutyte@Lorenass-MBP.lan>
2023-04-26 13:16:59 +0300

    modify Post component style

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Post.css';
 import { IoIosArrowRoundForward } from 'react-icons/io'
 
@@ -8,27 +8,32 @@ const Post = ({ data }) => {
     return (
         <>
             <div className="col-xs-12 col-lg-6 mb-2 mt-2 column">
-                <div className="blog">
-
-                    {/* image */}
-                    <div className="blog-img">
-                        <div className="date">{new Date(data.created_at).toLocaleDateString('en', {
+                <div className="blog-card">
+                    <div className="blog-card-img">
+                        <div className="date">{new Date(data.created_at).toLocaleDateString('en-GB', {
                             month: 'long',
                             day: 'numeric',
                             year: 'numeric'
                         })}</div>
-                        <Link to={'/posts/' + data.id}>
+                        <NavLink to={'/posts/' + data.id}>
                             <img src={data.image_path} alt={data.title} className="w-100" />
-                        </Link>
+                        </NavLink>
                     </div>
 
-                    <div className="blog-info">
-                        <h5><Link to={'/posts/' + data.id}>{data.title}</Link></h5>
-                        <p>{data.body.split(' ').splice(0, 15).join(' ') + ' [...]'}</p>
-                        <div className="read">
-                            <Link to={'/posts/' + data.id} className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                    <div className="blog-card-info">
+                        <div className='info-title'>
+                            <h5><NavLink to={'/posts/' + data.id}>{data.title}</NavLink></h5>
+                        </div>
+                        <div className='info-body'>
+                            <p>{data.body.split(' ').splice(0, 15).join(' ') + ' [...]'}</p>
+                        </div>
+                        <div className="read d-flex justify-content-between">
+                            <NavLink to={'/posts/' + data.id} className="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
                                 <span>Continue reading <IoIosArrowRoundForward /></span>
-                            </Link>
+                            </NavLink>
+                            <div>
+                                <span className='text-secondary'>{data.min_to_read} min read</span>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,6 +4,7 @@ import { LoadingContext } from './context/LoadingContext';
 import Post from './Post';
 
 const Posts = () => {
+
     const { setLoading } = useContext(LoadingContext)
     const [posts, setPosts] = useState([])
 
@@ -12,7 +13,6 @@ const Posts = () => {
 
         axios.get('http://127.0.0.1:8000/api/posts')
             .then(resp => {
-                // console.log(resp.data)
                 setPosts(resp.data)
             })
             .catch(error => {
@@ -21,30 +21,22 @@ const Posts = () => {
             .finally(() => setLoading(false))
     }, [])
 
+
     return (
         <>
-
-            <section className='section' id='blog'>
-                <div className='container'>
-
-                    <div className="row justify-content-center">
-                        {Array.isArray(posts)
-                            ? posts.map(data => {
-                                // return console.log(data.comments)
-                                // return console.log(data.attributes)
-                                return <Post
-                                    key={data.id}
-                                    data={data.attributes}
-                                // dataComments={data.comments}
-                                />
-                            })
-                            : null
-                        }
-                    </div>
+            <div className='container'>
+                <div className="row justify-content-center">
+                    {Array.isArray(posts)
+                        ? posts.map(data => {
+                            return <Post
+                                key={data.id}
+                                data={data.attributes}
+                            />
+                        })
+                        : null
+                    }
                 </div>
-            </section>
-
-
+            </div>
 
         </>
     );
